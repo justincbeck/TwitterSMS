dc97cd086786843f30e664a16d58a7c4bf9b0552
Justin Beck <justinbeck@mac.com>
2011-10-14 21:08:56 -0400

    Initial Commit

---
+++ WillowTree/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WillowTree
+//
+//  Created by Justin Beck on 10/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
